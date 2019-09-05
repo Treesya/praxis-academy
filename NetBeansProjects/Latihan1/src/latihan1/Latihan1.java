ed78131634d4f30cc78f2e428c0536a2374e2b99
treesya <treesyahm@gmail.com>
2019-09-05 14:39:44 +0700

    test

--- NetBeansProjects/Latihan1/src/latihan1/Latihan1.java
+++ NetBeansProjects/Latihan1/src/latihan1/Latihan1.java
@@ -1,8 +1,8 @@
-package oop;
+package Latihan1;
 
 import java.util.Scanner;
 
-public class Main {
+public class Latihan1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
