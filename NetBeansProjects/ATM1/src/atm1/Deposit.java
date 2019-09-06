c172fd42bbce54988d6e8c0ee74b5798b79e593f
treesya <treesyahm@gmail.com>
2019-09-06 18:24:10 +0700

    test

---
+++ NetBeansProjects/ATM1/src/atm1/Deposit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm1;
+
+/**
+ *
+ * @author user pc
+ */
+
+public class Deposit extends ATMMachine
+{
+    static double deposit;
+    public void setDeposit(double d)
+    {
+        deposit = d;
+    }
+    public static double getDeposit()
+    {
+        return deposit;
+    }
+}
