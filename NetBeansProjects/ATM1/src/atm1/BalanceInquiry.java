629cbed570580b9bb1e4730d4cf59075d71efb86
treesya <treesyahm@gmail.com>
2019-09-06 18:23:32 +0700

    test

---
+++ NetBeansProjects/ATM1/src/atm1/BalanceInquiry.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm1;
+
+/**
+ *
+ * @author user pc
+ */
+
+public class BalanceInquiry extends ATMMachine
+{
+    static double balance = 0;
+    public void setBalance(double b)
+    {
+        balance = b;
+    }
+    public static double getBalance()
+    {
+        return balance;
+    }     
+}
